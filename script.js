ed505435de2a71b615d0fa2044f219603f5622ba
Yoshiaki Togami <tuabobo123@gmail.com>
2021-03-13 21:57:20 +0900

    Enable to install

---
+++ script.js
@@ -0,0 +1,34 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./service-worker.js")
+    .then(function (registration) {
+      console.log("Registration successful, scope is:", registration.scope);
+    })
+    .catch(function (error) {
+      console.log("Service worker registration failed, error:", error);
+    });
+}
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  console.log("👍", "beforeinstallprompt", event);
+  window.deferredPrompt = event;
+  divInstall.classList.toggle("hidden", false);
+});
+
+butInstall.addEventListener("click", async () => {
+  console.log("👍", "butInstall-clicked");
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    return;
+  }
+  promptEvent.prompt();
+  const result = await promptEvent.userChoice;
+  console.log("👍", "userChoice", result);
+  window.deferredPrompt = null;
+  divInstall.classList.toggle("hidden", true);
+});
+
+window.addEventListener("appinstalled", (event) => {
+  console.log("👍", "appinstalled", event);
+  window.deferredPrompt = null;
+});
