f3b82af0e3df562efad0e6286617cce72a25b357
Yoshiaki Togami <tuabobo123@gmail.com>
2021-03-20 15:50:48 +0900

    fix

--- script.js
+++ script.js
@@ -12,21 +12,21 @@ if ("serviceWorker" in navigator) {
 window.addEventListener("beforeinstallprompt", (event) => {
   console.log("👍", "beforeinstallprompt", event);
   window.deferredPrompt = event;
-  divInstall.classList.toggle("hidden", false);
+  // divInstall.classList.toggle("hidden", false);
 });
 
-butInstall.addEventListener("click", async () => {
-  console.log("👍", "butInstall-clicked");
-  const promptEvent = window.deferredPrompt;
-  if (!promptEvent) {
-    return;
-  }
-  promptEvent.prompt();
-  const result = await promptEvent.userChoice;
-  console.log("👍", "userChoice", result);
-  window.deferredPrompt = null;
-  divInstall.classList.toggle("hidden", true);
-});
+// butInstall.addEventListener("click", async () => {
+//   console.log("👍", "butInstall-clicked");
+//   const promptEvent = window.deferredPrompt;
+//   if (!promptEvent) {
+//     return;
+//   }
+//   promptEvent.prompt();
+//   const result = await promptEvent.userChoice;
+//   console.log("👍", "userChoice", result);
+//   window.deferredPrompt = null;
+//   divInstall.classList.toggle("hidden", true);
+// });
 
 window.addEventListener("appinstalled", (event) => {
   console.log("👍", "appinstalled", event);
@@ -35,19 +35,9 @@ window.addEventListener("appinstalled", (event) => {
 
 //送信
 const button = document.getElementById("send");
-const receiver = document.getElementById("recieve");
 button.addEventListener("click", () => {
   console.log("clicked");
   var editorExtensionId = " cchlfodlokmandokbcbdnccndllpkiak";
   const message = "アプリからのメッセージです";
   chrome.runtime.sendMessage(editorExtensionId, message);
-});
-
-//受信
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log("メッセージを拡張から取得しました。");
-  receiver.innerHTML = "success!!";
-  sendResponse({
-    success: true,
-  });
 });
